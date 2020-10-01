2a2b7f62640ab2fcfef2573980b8ac38a1e81c4e
Bertrand Fournel <fournel.bertrand@gmail.com>
2020-10-01 22:19:37 +0200

    deployment on github

--- app.py
+++ app.py
@@ -17,13 +17,20 @@ model = load('model.joblib')
 
 @app.route('/get_result', methods=['POST'])
 def get_result():
+
+    #On récupère les données depuis javascript
     data = request.data
+
+    #On convertit les données en une string
     data_str = str(data)
 
-
+    # On localise le début des données d'image (on délaisse la string : data:image/png;base64)
     offset = data_str.index(',')+1
 
+    # On transforme en nouvel objet bytes-like
     img_bytes = base64.b64decode(data_str[offset:])
+
+    # On récupère l'image dans un objet Image de Pillow
     img = Image.open(BytesIO(img_bytes))
     
     #On convertit l'image en niveaux de gris
